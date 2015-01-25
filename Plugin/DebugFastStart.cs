0f320da18ad936c38aac9c68269595e8090b711d
Youen Toupin <neuoy@users.noreply.github.com>
2015-01-25 16:52:04 +0100

    DebugFastStart now starts the tracking station so that it's possible to choose a vessel (and also because this way FAR is correctly initialized)

--- Plugin/DebugFastStart.cs
+++ Plugin/DebugFastStart.cs
@@ -9,7 +9,7 @@ namespace Trajectories
 {
 #if DEBUG
     //This will kick us into the save called default and set the first vessel active
-    [KSPAddon(KSPAddon.Startup.MainMenu, false)]
+    [KSPAddon(KSPAddon.Startup.EveryScene, false)]
     public class DebugFastStart : MonoBehaviour
     {
         [DllImport("user32.dll", EntryPoint = "SetWindowText")]
@@ -18,51 +18,68 @@ namespace Trajectories
         private static extern System.IntPtr FindWindow(System.String className, System.String windowName);
 
         //use this variable for first run to avoid the issue with when this is true and multiple addons use it
-        public static bool first = true;
+        private static bool first = true;
         public void Start()
         {
-            var windowPtr = FindWindow(null, "Kerbal Space Program");
-            if(windowPtr != null)
-                SetWindowText(windowPtr, "KSP - Trajectories Debug"); // this is handy to have AutoSizer or similar program correctly set the window position when launching
-
-            if (AppDomain.CurrentDomain.GetAssemblies().Any(a => a.FullName.ToLower().Contains("testautomation")))
-                return;
-
-            //only do it on the first entry to the menu
-            if (first)
+            if(HighLogic.LoadedScene == GameScenes.MAINMENU)
             {
-                first = false;
-                HighLogic.SaveFolder = "default";
-                Game game = GamePersistence.LoadGame("persistent", HighLogic.SaveFolder, true, false);
-
-                if (game != null && game.flightState != null && game.compatible)
+                //only do it on the first entry to the menu
+                if (first)
                 {
-                    Int32 FirstVessel;
-                    Boolean blnFoundVessel = false;
-                    for (FirstVessel = 0; FirstVessel < game.flightState.protoVessels.Count; FirstVessel++)
-                    {
-                        //This logic finds the first non-asteroid vessel
-                        if (game.flightState.protoVessels[FirstVessel].vesselType != VesselType.SpaceObject &&
-                            game.flightState.protoVessels[FirstVessel].vesselType != VesselType.Unknown &&
-                            game.flightState.protoVessels[FirstVessel].vesselType != VesselType.Flag)
-                        {
-                            ////////////////////////////////////////////////////
-                            //PUT ANY OTHER LOGIC YOU WANT IN HERE//
-                            ////////////////////////////////////////////////////
-                            blnFoundVessel = true;
-                            break;
-                        }
-                    }
-                    if (!blnFoundVessel)
-                        FirstVessel = 0;
+                    first = false;
 
-                    // warning : with KSP 0.90 and new NEAR version, skipping the KSC screen prevents NEAR from initializing correctly, so we can't directly load a vessel
-                    //FlightDriver.StartAndFocusVessel(game, FirstVessel);
+                    var windowPtr = FindWindow(null, "Kerbal Space Program");
+                    if (windowPtr != null)
+                        SetWindowText(windowPtr, "KSP - Trajectories Debug"); // this is handy to have AutoSizer or similar program correctly set the window position when launching
+
+                    if (AppDomain.CurrentDomain.GetAssemblies().Any(a => a.FullName.ToLower().Contains("testautomation")))
+                        return;
+
+                    HighLogic.SaveFolder = "default";
+                    HighLogic.LoadScene(GameScenes.TRACKSTATION);
+
+                    CheatOptions.InfiniteFuel = true;
                 }
-
-                CheatOptions.InfiniteFuel = true;
             }
         }
+
+        /*private static int loadedVessel = 100;
+        public void Update()
+        {
+            Util.PostSingleScreenMessage("debug fast start", "count: " + loadedVessel);
+            if (HighLogic.LoadedScene == GameScenes.SPACECENTER && loadedVessel > 0)
+            {
+                --loadedVessel;
+                if (loadedVessel == 0)
+                {
+                    Game game = GamePersistence.LoadGame("persistent", HighLogic.SaveFolder, true, false);
+
+                    if (game != null && game.flightState != null && game.compatible)
+                    {
+                        Int32 FirstVessel;
+                        Boolean blnFoundVessel = false;
+                        for (FirstVessel = 0; FirstVessel < game.flightState.protoVessels.Count; FirstVessel++)
+                        {
+                            //This logic finds the first non-asteroid vessel
+                            if (game.flightState.protoVessels[FirstVessel].vesselType != VesselType.SpaceObject &&
+                                game.flightState.protoVessels[FirstVessel].vesselType != VesselType.Unknown &&
+                                game.flightState.protoVessels[FirstVessel].vesselType != VesselType.Flag)
+                            {
+                                ////////////////////////////////////////////////////
+                                //PUT ANY OTHER LOGIC YOU WANT IN HERE//
+                                ////////////////////////////////////////////////////
+                                blnFoundVessel = true;
+                                break;
+                            }
+                        }
+                        if (!blnFoundVessel)
+                            FirstVessel = 0;
+
+                        FlightDriver.StartAndFocusVessel(game, FirstVessel);
+                    }
+                }
+            }
+        }*/
     }
 #endif
 }
