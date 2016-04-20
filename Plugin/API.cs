f515332c44db0584ae90c7c73ffa1ceac61bb4cb
CalebJ2 <calebinvents@gmail.com>
2016-04-19 21:26:20 -0700

    Added basic API

---
+++ Plugin/API.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+namespace Trajectories
+{
+    public static class API
+    {
+
+        public static bool APIAvailable()
+        {
+            return true;
+        }
+        public static Vector3? getImpactPosition()
+        {
+            Trajectory trajectory = Trajectory.fetch;
+            trajectory.ComputeTrajectory(FlightGlobals.ActiveVessel, DescentProfile.fetch, true);
+            foreach (var patch in trajectory.patches)
+            {
+                if (patch.impactPosition != null)
+                {
+                    return patch.impactPosition;
+                }
+            }
+            return null;
+        }
+    }
+}
+
