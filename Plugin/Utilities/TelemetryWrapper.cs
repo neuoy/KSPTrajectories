3da6dcf6a8ab1f03daf76bbd0daa1509e8fdfe01
Alexander Korsunsky <fat.lobyte9@gmail.com>
2017-06-22 19:27:36 +0200

    Add TelemetryWrapper
    The Telemetry Plugins allows logging of numeric values during flight.
    The TelemetryWrapper class is the interface to that module

---
+++ Plugin/Utilities/TelemetryWrapper.cs
@@ -0,0 +1,71 @@
+﻿#if DEBUG_TELEMETRY
+
+using System;
+using System.Reflection;
+using UnityEngine;
+
+namespace Trajectories
+{
+    public class Telemetry
+    {
+        private static string thisAssemblyName = Assembly.GetExecutingAssembly().GetName().Name;
+
+        private static object telemetryServiceInstance = null;
+        private static MethodInfo addChannelMethod;
+        private static MethodInfo sendMethod;
+
+
+        public static void AddChannel<ChannelType>(string id, string format = null)
+            where ChannelType : IFormattable
+        {
+            if (telemetryServiceInstance == null)
+                return;
+
+            // AddChannel is generic, we find the concrete implementation
+            MethodInfo addChannelMethodConcrete =
+                addChannelMethod.MakeGenericMethod(typeof(ChannelType));
+
+            var parameters = new object[] { thisAssemblyName + "/" + id, format };
+            addChannelMethodConcrete.Invoke(telemetryServiceInstance, parameters);
+        }
+
+        public static void Send(string id, object value)
+        {
+            if (telemetryServiceInstance == null)
+                return;
+
+            var parameters = new object[] { thisAssemblyName + "/" + id, value };
+            sendMethod.Invoke(telemetryServiceInstance, parameters);
+        }
+
+
+        static Telemetry()
+        {
+            // Search for telemetry assembly
+            Type telemetryServiceType = null;
+            foreach (var loadedAssembly in AssemblyLoader.loadedAssemblies)
+            {
+                if (loadedAssembly.name != "Telemetry")
+                    continue;
+
+                telemetryServiceType = loadedAssembly.assembly.GetType("Telemetry.TelemetryService");
+            }
+
+            // if it's not loaded, bow and excuse ourselves
+            if (telemetryServiceType == null)
+            {
+                Debug.Log(thisAssemblyName + " could not find Telemetry module. Continuing without Telemetry.");
+                return;
+            }
+
+            // if it's loaded, get instance and Send/AddChannel Methods
+            telemetryServiceInstance = telemetryServiceType.GetProperty("Instance", telemetryServiceType).GetValue(null, null);
+            addChannelMethod = telemetryServiceType.GetMethod("AddChannel");
+            sendMethod = telemetryServiceType.GetMethod("Send");
+
+            Debug.Log(thisAssemblyName + " connected to Telemetry module.");
+        }
+    }
+}
+
+#endif
