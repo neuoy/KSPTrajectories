d2b28fddf04a5ac079860592634f5b6d213e462c
PiezPiedPy <githubpie@maxautodesign.com>
2017-06-24 05:38:13 +0100

    Changed Profiler to use KSP's PopupDialog class
    The Profiler GUI has been updated to use KSP's PopupDialog class which
    in turn uses a GameObject with a canvas and also has the added benefit
    of stopping most mouse click through events.

--- Plugin/Utilities/Profiler.cs
+++ Plugin/Utilities/Profiler.cs
@@ -2,237 +2,214 @@
 
 using System;
 using System.Collections.Generic;
-using System.Diagnostics;
+using KSP.Localization;
 using UnityEngine;
 
 
 namespace Trajectories
 {
-
-
-  [KSPAddon(KSPAddon.Startup.MainMenu, true)]
-  public sealed class Profiler: MonoBehaviour
-  {
-    // constants
-    const float width = 400.0f;
-    const float height = 500.0f;
-    const float top_height = 20.0f;
-    const float bot_height = 20.0f;
-    const float margin = 10.0f;
-    const float spacing = 10.0f;
-
-    // permit global access
-    private static Profiler instance = null;
-
-    // styles
-    GUIStyle win_style;
-    GUIStyle top_style;
-    GUIStyle name_style;
-    GUIStyle value_style;
-
-    // store window id
-    int win_id;
-
-    // store window geometry
-    Rect win_rect;
-
-    // store dragbox geometry
-    Rect drag_rect;
-
-    // used by scroll window mechanics
-    Vector2 scroll_pos;
-
-    // visible flag
-    bool visible;
-
-    // an entry in the profiler
-    class entry
+    /// <summary> Simple profiler for measuring the execution time of code placed between the Start and Stop methods. </summary>
+    [KSPAddon(KSPAddon.Startup.MainMenu, true)]
+    public sealed class Profiler: MonoBehaviour
     {
-      public UInt64 start;        // used to measure call time
-      public UInt64 calls;        // number of calls in current simulation step
-      public UInt64 time;         // time in current simulation step
-      public UInt64 prev_calls;   // number of calls in previous simulation step
-      public UInt64 prev_time;    // time in previous simulation step
-      public UInt64 tot_calls;    // number of calls in total
-      public UInt64 tot_time;     // total time
+        // constants
+        private const float width = 400.0f;
+        private const float height = 500.0f;
 
-    }
+        private const float value_width = 65.0f;
 
-    // store all entries
-    Dictionary<string, entry> entries = new Dictionary<string, entry>();
+        // permit global access
+        private static Profiler instance = null;
 
+        // visible flag
+        private static bool visible;
 
-    // ctor
-    Profiler()
-    {
-      // enable global access
-      instance = this;
+        // popup window 
+        private static MultiOptionDialog multi_dialog;
+        private static PopupDialog popup_dialog;
+        private static DialogGUIVerticalLayout dialog_items;
 
-      // generate unique id, hopefully
-      win_id = Util.RandomInt(int.MaxValue);
+        // an entry in the profiler
+        private class entry
+        {
+            public double start;        // used to measure call time
+            public double calls;        // number of calls in current simulation step
+            public double time;         // time in current simulation step
+            public double prev_calls;   // number of calls in previous simulation step
+            public double prev_time;    // time in previous simulation step
+            public double tot_calls;    // number of calls in total
+            public double tot_time;     // total time
+        }
 
-      // setup window geometry
-      win_rect = new Rect((Screen.width - width) * 0.5f, (Screen.height - height) * 0.5f, width, height);
-
-      // setup dragbox geometry
-      drag_rect = new Rect(0.0f, 0.0f, width, top_height);
-
-      // setup styles
-      win_style = new GUIStyle(HighLogic.Skin.window);
-      top_style = new GUIStyle();
-      name_style = new GUIStyle();
-      value_style = new GUIStyle();
-    }
-
-    //  Awake is called only once when the script instance is being loaded. Used in place of the constructor for initialization.
-    public void Awake()
-    {
-      // keep it alive
-      DontDestroyOnLoad(this);
-
-      // setup styles
-      win_style.padding.top = 0;
-      win_style.padding.bottom = 0;
-      top_style.fixedHeight = top_height;
-      top_style.fontStyle = FontStyle.Bold;
-      top_style.alignment = TextAnchor.MiddleCenter;
-      name_style.fontSize = 10;
-      name_style.fixedWidth = 150.0f;
-      name_style.stretchWidth = false;
-      value_style.fontSize = 10;
-      value_style.fixedWidth = 75.0f;
-      value_style.stretchWidth = false;
-      value_style.alignment = TextAnchor.MiddleRight;
-    }
-
-    // called every frame
-    public void OnGUI()
-    {
-      if (Util.IsGame() && visible)
-      {
-        // clamp the window to the screen, so it can't be dragged outside
-        float offset_x = Math.Max(0.0f, -win_rect.xMin) + Math.Min(0.0f, Screen.width - win_rect.xMax);
-        float offset_y = Math.Max(0.0f, -win_rect.yMin) + Math.Min(0.0f, Screen.height - win_rect.yMax);
-        win_rect.xMin += offset_x;
-        win_rect.xMax += offset_x;
-        win_rect.yMin += offset_y;
-        win_rect.yMax += offset_y;
-
-        // draw the window
-        win_rect = GUILayout.Window(win_id, win_rect, render, "", win_style);
-      }
-    }
+        // store all entries
+        private Dictionary<string, entry> entries = new Dictionary<string, entry>();
 
 
-    // draw the window
-    void render(int id)
-    {
-      // draw pseudo-title
-      GUILayout.BeginHorizontal();
-      GUILayout.Label("Profiler", top_style);
-      GUILayout.EndHorizontal();
+        public static Profiler Instance
+        {
+            get
+            {
+                return instance;
+            }
+        }
 
-      // draw top spacing
-      GUILayout.Space(spacing);
+        //  constructor
+        public Profiler()
+        {
+            // enable global access
+            instance = this;
 
-      // draw entries
-      scroll_pos = GUILayout.BeginScrollView(scroll_pos, HighLogic.Skin.horizontalScrollbar, HighLogic.Skin.verticalScrollbar);
-      GUILayout.BeginHorizontal();
-      GUILayout.Label("<b>NAME</b>", name_style);
-      GUILayout.Label("<b>LAST</b>", value_style);
-      GUILayout.Label("<b>AVG</b>", value_style);
-      GUILayout.Label("<b>CALLS</b>", value_style);
-      GUILayout.EndHorizontal();
-      foreach (var pair in entries)
-      {
-        string e_name = pair.Key;
-        entry e = pair.Value;
-        GUILayout.BeginHorizontal();
-        GUILayout.Label(e_name, name_style);
-        GUILayout.Label(e.prev_calls > 0 ? Util.Microseconds(e.prev_time / e.prev_calls).ToString("F2") : "", value_style);
-        GUILayout.Label(e.tot_calls > 0 ? Util.Microseconds(e.tot_time / e.tot_calls).ToString("F2") : "", value_style);
-        GUILayout.Label(e.prev_calls.ToString(), value_style);
-        GUILayout.EndHorizontal();
-      }
-      GUILayout.EndScrollView();
+            // create window
+            dialog_items = new DialogGUIVerticalLayout();
+            multi_dialog = new MultiOptionDialog(
+               "TrajectoriesProfilerWindow",
+               "",
+               GetTitle(),
+               HighLogic.UISkin,
+               new Rect(0.5f, 0.5f, width, height),
+               new DialogGUIBase[]
+               {
+                   // create header line
+                   new DialogGUIHorizontalLayout(
+                       new DialogGUILabel("<b>   NAME</b>", true),
+                       new DialogGUILabel("<b>LAST</b>", value_width),
+                       new DialogGUILabel("<b>AVG</b>", value_width),
+                       new DialogGUILabel("<b>CALLS</b>", value_width - 15)),
+                   // create scrollbox for entry data
+                   new DialogGUIScrollList(new Vector2(), false, true, dialog_items)
+               });
+        }
 
-      // draw bottom spacing
-      GUILayout.Space(spacing);
+        // Awake is called only once when the script instance is being loaded. Used in place of the constructor for initialization.
+        private void Awake()
+        {
+            // keep it alive
+            DontDestroyOnLoad(this);
 
-      // enable dragging
-      GUI.DragWindow(drag_rect);
-    }
+            // create popup dialog
+            popup_dialog = PopupDialog.SpawnPopupDialog(multi_dialog, true, HighLogic.UISkin, false, "");
+            if (popup_dialog != null)
+                popup_dialog.gameObject.SetActive(false);
+        }
 
+        private void Update()
+        {
+            if ((Input.GetKey(KeyCode.LeftControl) || Input.GetKey(KeyCode.RightControl)) &&
+                     Input.GetKeyUp(KeyCode.P) && popup_dialog != null)
+            {
+                visible = !visible;
+                popup_dialog.gameObject.SetActive(visible);
+            }
+        }
 
-    public void Update()
-    {
-      if ((Input.GetKey(KeyCode.LeftControl) || Input.GetKey(KeyCode.RightControl)) && Input.GetKeyUp(KeyCode.P))
-      {
-        visible = !visible;
-      }
-    }
+        private void FixedUpdate()
+        {
+            foreach (var p in entries)
+            {
+                entry e = p.Value;
+                e.prev_calls = e.calls;
+                e.prev_time = e.time;
+                e.tot_calls += e.calls;
+                e.tot_time += e.time;
+                e.calls = 0;
+                e.time = 0;
+            }
+        }
 
+        private void OnDestroy()
+        {
+            instance = null;
+            popup_dialog.Dismiss();
+            popup_dialog = null;
+        }
 
-    public void FixedUpdate()
-    {
-      foreach (var p in entries)
-      {
-        entry e = p.Value;
-        e.prev_calls = e.calls;
-        e.prev_time = e.time;
-        e.tot_calls += e.calls;
-        e.tot_time += e.time;
-        e.calls = 0;
-        e.time = 0;
-      }
-    }
+        private static string GetTitle()
+        {
+            switch (Localizer.CurrentLanguage)
+            {
+                case "es-es":
+                    return "Trayectorias Profiler";
+                case "ru":
+                    return "Провайдер траектории";
+                case "zh-cn":
+                    return "軌跡分析儀";
+                default:
+                    return "Trajectories Profiler";
+            }
+        }
 
+        private void AddDialogItem(string e_name)
+        {
+            // add item
+            dialog_items.AddChild(
+                new DialogGUIHorizontalLayout(
+                    new DialogGUILabel("  " + e_name, true),
+                    new DialogGUILabel(() =>
+                    {
+                        return (entries[e_name].prev_calls > 0 ? Util.Microseconds(entries[e_name].prev_time /
+                                    entries[e_name].prev_calls).ToString("F2") : "") + "ms";
+                    }, value_width),
+                    new DialogGUILabel(() =>
+                    {
+                        return (entries[e_name].tot_calls > 0 ? Util.Microseconds(entries[e_name].tot_time /
+                                    entries[e_name].tot_calls).ToString("F2") : "") + "ms";
+                    }, value_width),
+                    new DialogGUILabel(() =>
+                    {
+                        return entries[e_name].prev_calls.ToString();
+                    }, value_width - 15)));
 
-    // start an entry
-    public static void Start(string e_name)
-    {
-      if (instance == null)
-        return;
+            // required to force the Gui creation
+            Stack<Transform> stack = new Stack<Transform>();
+            stack.Push(dialog_items.uiItem.gameObject.transform);
+            dialog_items.children[dialog_items.children.Count - 1].Create(ref stack, HighLogic.UISkin);
+        }
 
-      if (!instance.entries.ContainsKey(e_name))
-        instance.entries.Add(e_name, new entry());
+        /// <summary> Start a profiler entry. </summary>
+        public static void Start(string e_name)
+        {
+            if (instance == null)
+                return;
 
-      entry e = instance.entries[e_name];
-      e.start = Util.Clocks();
+            if (!instance.entries.ContainsKey(e_name))
+            {
+                instance.entries.Add(e_name, new entry());
+                instance.AddDialogItem(e_name);
+            }
+
+            instance.entries[e_name].start = Util.Clocks;
+        }
+
+        /// <summary> Stop a profiler entry. </summary>
+        public static void Stop(string e_name)
+        {
+            if (instance == null)
+                return;
+
+            entry e = instance.entries[e_name];
+
+            ++e.calls;
+            e.time += Util.Clocks - e.start;
+        }
     }
 
 
-    // stop an entry
-    public static void Stop(string e_name)
+    /// <summary> Profile a function scope. </summary>
+    public class ProfileScope: IDisposable
     {
-      if (instance == null)
-        return;
+        public ProfileScope(string name)
+        {
+            this.name = name;
+            Profiler.Start(name);
+        }
 
-      entry e = instance.entries[e_name];
+        public void Dispose()
+        {
+            Profiler.Stop(name);
+        }
 
-      ++e.calls;
-      e.time += Util.Clocks() - e.start;
+        private string name;
     }
-  }
-
-
-  // profile a function scope
-  public class ProfileScope: IDisposable
-  {
-    public ProfileScope(string name)
-    {
-      this.name = name;
-      Profiler.Start(name);
-    }
-
-    public void Dispose()
-    {
-      Profiler.Stop(name);
-    }
-
-
-    private string name;
-  }
 
 
 } // Trajectories
