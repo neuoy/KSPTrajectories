88d8da5e28ff9d7b9f2492e472327af2712ebe39
PiezPiedPy <githubpie@maxautodesign.com>
2017-07-04 02:32:32 +0100

    Update MM to v2.8.1

--- misc/VisualStudio/buildscripts/ZipPack/build-package.bat
+++ misc/VisualStudio/buildscripts/ZipPack/build-package.bat
@@ -14,7 +14,7 @@ SET rootPath=%scriptPath%..\..\..\..\
 SET initialWD=%CD%
 
 rem current module manager version in misc\3rdParty\
-SET MODMANAGER_VERSION=2.8.0
+SET MODMANAGER_VERSION=2.8.1
 
 echo Generating %TargetName% Release Package...
 cd "%rootPath%"
