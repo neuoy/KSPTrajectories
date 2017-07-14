24ea59696f72896a6855aded200814d7583f4ada
Alexander Korsunsky <fat.lobyte9@gmail.com>
2017-07-14 15:41:21 +0200

    FlightOverlay: remove line drawing with LineRenderer

--- Plugin/FlightOverlay.cs
+++ Plugin/FlightOverlay.cs
@@ -9,32 +9,24 @@ namespace Trajectories
         private const int defaultVertexCount = 32;
         private const float lineWidth = 2.0f;
 
-        private LineRenderer line { get; set; }
-
-        private TrajectoryLine trajectoryLine;
+        private TrajectoryLine line;
 
         private TargetingCross targetingCross;
 
         public void Awake()
         {
-            trajectoryLine = FlightCamera.fetch.mainCamera.gameObject.AddComponent<TrajectoryLine>();
+            line = FlightCamera.fetch.mainCamera.gameObject.AddComponent<TrajectoryLine>();
             targetingCross = FlightCamera.fetch.mainCamera.gameObject.AddComponent<TargetingCross>();
         }
 
         public void Start()
         {
-            line = gameObject.AddComponent<LineRenderer>();
-            line.useWorldSpace = false; // true;
-            line.SetVertexCount(defaultVertexCount);
-            line.SetWidth(lineWidth, lineWidth);
-            line.sharedMaterial = Resources.Load("DefaultLine3D") as Material;
-            line.material.SetColor("_TintColor", new Color(0.1f, 1f, 0.1f));
+
         }
 
         private void FixedUpdate()
         {
             line.enabled = false;
-            trajectoryLine.enabled = false;
             targetingCross.enabled = false;
 
             if (!Settings.fetch.DisplayTrajectories
@@ -43,26 +35,20 @@ namespace Trajectories
                 || Trajectory.fetch.patches.Count == 0)
                 return;
 
-            Vector3[] vertices;
-            trajectoryLine.Vertices.Clear();
+            line.Vertices.Clear();
 
             Trajectory.Patch lastPatch = Trajectory.fetch.patches[Trajectory.fetch.patches.Count - 1];
             Vector3d bodyPosition = lastPatch.startingState.referenceBody.position;
             if (lastPatch.isAtmospheric)
             {
-                vertices = new Vector3[lastPatch.atmosphericTrajectory.Length];
-
                 for (uint i = 0; i < lastPatch.atmosphericTrajectory.Length; ++i)
                 {
                     Vector3 vertex = lastPatch.atmosphericTrajectory[i].pos + bodyPosition;
-                    vertices[i] = vertex;
-                    trajectoryLine.Vertices.Add(vertices[i]);
+                    line.Vertices.Add(vertex);
                 }
             }
             else
             {
-                vertices = new Vector3[defaultVertexCount];
-
                 double time = lastPatch.startingState.time;
                 double time_increment = (lastPatch.endTime - lastPatch.startingState.time) / defaultVertexCount;
                 Orbit orbit = lastPatch.spaceOrbit;
@@ -74,21 +60,14 @@ namespace Trajectories
 
                     vertex += bodyPosition;
 
-                    vertices[i] = vertex;
-                    trajectoryLine.Vertices.Add(vertex);
+                    line.Vertices.Add(vertex);
 
                     time += time_increment;
                 }
             }
 
-            // add vertices to line
-            line.SetVertexCount(vertices.Length);
-            line.SetPositions(vertices);
-
+            line.Body = lastPatch.startingState.referenceBody;
             line.enabled = true;
-
-            trajectoryLine.Body = lastPatch.startingState.referenceBody;
-            trajectoryLine.enabled = true;
 
             if (lastPatch.impactPosition != null)
             {
@@ -102,12 +81,6 @@ namespace Trajectories
                 targetingCross.ImpactBody = null;
             }
         }
-
-        public void OnDestroy()
-        {
-            if (line != null)
-                Destroy(line);
-        }
     }
 
     public class TrajectoryLine: MonoBehaviour
@@ -120,7 +93,7 @@ namespace Trajectories
             if (Body == null || Vertices == null || Vertices.Count == 0)
                 return;
 
-            GLUtils.DrawPath(Body, Vertices, Color.blue, false, true);
+            GLUtils.DrawPath(Body, Vertices, Color.blue, false, false);
         }
     }
 
