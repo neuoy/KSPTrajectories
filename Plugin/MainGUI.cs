b779bfd9fe70e6a67441722ed99ca740c39c729e
PiezPiedPy <githubpie@maxautodesign.com>
2017-10-18 21:02:58 +0100

    Created MainGUI.cs to hold the MainGUI class.

---
+++ Plugin/MainGUI.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace Trajectories
+{
+    /// <summary> MainGUI window handler. </summary>
+    [KSPAddon(KSPAddon.Startup.Flight, false)]
+    public class MainGUI: MonoBehaviour
+    {
+    }
+}
