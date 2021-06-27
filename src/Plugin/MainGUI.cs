bf8e3559bc0903bcba2f5a754642f60ee111dae1
Alexander Korsunsky <fat.lobyte9@gmail.com>
2021-06-27 17:09:05 +0200

    Splashed down vessel can now also be targeted

--- src/Plugin/MainGUI.cs
+++ src/Plugin/MainGUI.cs
@@ -754,7 +754,7 @@ namespace Trajectories
             // grab the currently targeted vessel
             Vessel targetVessel = FlightGlobals.fetch.VesselTarget?.GetVessel();
 
-            if (targetVessel != null && targetVessel.Landed)
+            if (targetVessel != null && targetVessel.LandedOrSplashed)
                 return true;
             return false;
         }
@@ -919,7 +919,7 @@ namespace Trajectories
             // grab the currently targeted vessel
             Vessel targetVessel = FlightGlobals.fetch.VesselTarget?.GetVessel();
 
-            if (targetVessel != null && targetVessel.Landed)
+            if (targetVessel != null && targetVessel.LandedOrSplashed)
             {
                 TargetProfile.SetFromWorldPos(targetVessel.lastBody, targetVessel.GetWorldPos3D() - targetVessel.lastBody.position);
                 ScreenMessages.PostScreenMessage(Localizer.Format("#autoLOC_Trajectories_TargetingVessel", targetVessel.GetName()));
