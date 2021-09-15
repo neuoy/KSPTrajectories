59d6f97bba4e2e176a70e169282320c88d6e1e07
PiezPiedPy <githubpie@maxautodesign.com>
2021-09-15 19:56:41 +0100

    Cache: Add OrbitInfo class

---
+++ src/Plugin/Cache/OrbitInfo.cs
@@ -0,0 +1,42 @@
+﻿/*
+  Copyright© (c) 2017-2021 S.Gray, (aka PiezPiedPy).
+
+  This file is part of Trajectories.
+  Trajectories is available under the terms of GPL-3.0-or-later.
+  See the LICENSE.md file for more details.
+
+  Trajectories is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  Trajectories is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+  You should have received a copy of the GNU General Public License
+  along with Trajectories.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Trajectories
+{
+    /// <summary> Contains copied game data that can be used outside the Unity main thread. All needed data must be copied and not referenced. </summary>
+    internal static partial class GameDataCache
+    {
+        internal class OrbitInfo
+        {
+            // constructors
+            internal OrbitInfo()
+            {
+            }
+
+            internal OrbitInfo(Orbit orbit)
+            {
+            }
+        }
+    }
+}
